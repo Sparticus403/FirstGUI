a6c693f2b6e3be9b194b9ecd84e4cda773cb6a2a
Sparticus403 <twil8980@740s-pl-332.local>
2016-11-01 09:10:20 -0600

    added a color changer

--- src/graphic/view/FirstPanel.java
+++ src/graphic/view/FirstPanel.java
@@ -2,15 +2,53 @@ package graphic.view;
 
 import javax.swing.JPanel;
 import graphic.controller.FirstGUIController;
+import java.awt.Color;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FirstPanel extends JPanel
 {
 
 	private FirstGUIController baseController;
+	private JButton colorButton;
+	private JButton randomButton;
 	
 	public FirstPanel(FirstGUIController baseController)
 	{
 		super();
 		this.baseController = baseController;
+		
+		colorButton = new JButton("Color!");
+		randomButton = new JButton("Random");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setBackground(Color.BLUE);
+		this.add(colorButton);
+		this.add(randomButton);
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		colorButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				setBackground(Color.RED);
+			}
+
+		});
 	}
 }
